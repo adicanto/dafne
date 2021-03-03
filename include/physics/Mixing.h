a616a092b719dd4730e0508a91a967ca6749cd74
ataketa <edyedy603@hotmail.com>
2021-03-03 19:38:06 +0800

    add fit-KSpipi-time-dependent-time-resolution

--- include/physics/Mixing.h
+++ include/physics/Mixing.h
@@ -2,6 +2,9 @@
 
 #include <hydra/Function.h>
 #include <hydra/Complex.h>
+#include <hydra/functions/JohnsonSUShape.h>
+
+#include "RooMathM.h"
 
 namespace dafne {
 
@@ -108,4 +111,355 @@ auto MixingGm(hydra::Parameter const& tau, hydra::Parameter const& x, hydra::Par
 	return MixingG<true,Time>(tau,x,y);
 }
 
+
+
+typedef enum MixingPsiTag_t { PSI_P=+1, PSI_M=-1, PSI_I=0 } MixingPsiTag;
+
+template<typename T>
+__hydra_dual__ inline
+T _phi(T z)
+{
+	return exp(std::norm(z)/2.) * 1./2. * RooMathM::erfc(-z/sqrt(2.));
+}
+
+template<typename T>
+__hydra_dual__ inline
+T _psi(double x, T kappa)
+{
+	return exp((-2*x*kappa+kappa*kappa)/2.) * 1./2. * (1. + RooMathM::erf((x - kappa)/sqrt(2.)));
+}
+
+template<typename T>
+__hydra_dual__ inline
+T _int_psi_dt(double sigma, double x, T kappa)
+{
+	return sigma/kappa * 0.5 * (RooMathM::erf(x/sqrt(2.)) - 2.*_psi(x,kappa)) ;
+}
+
+template<MixingPsiTag Tag, typename Time, typename TimeError, typename Signature=hydra::complex<double>(Time,TimeError)>
+class MixingPsi: public hydra::BaseFunctor<MixingPsi<Tag,Time,TimeError>, Signature, 4>
+{
+	typedef hydra::BaseFunctor<MixingPsi<Tag,Time,TimeError>, Signature, 4> super_type;
+	using super_type::_par;
+
+private:
+	__hydra_dual__ inline
+	double suffix() const { return _par[0]; }
+
+	__hydra_dual__ inline
+	double tau() const { return _par[1]; }
+
+	__hydra_dual__ inline
+	double Gamma() const { return 1./_par[1]; }
+
+	__hydra_dual__ inline
+	double s() const { return _par[2]; }
+
+	__hydra_dual__ inline
+	double b() const { return _par[3]; }
+
+public:
+	MixingPsi() = delete;
+     
+	__hydra_dual__
+	MixingPsi(hydra::Parameter const& suffix, hydra::Parameter const& tau, hydra::Parameter const& s, hydra::Parameter const& b) : super_type({suffix,tau,s,b})
+	{}
+	
+	__hydra_dual__
+	MixingPsi(MixingPsi<Tag,Time,TimeError> const& other) : super_type(other)
+	{}
+	
+	__hydra_dual__ inline
+	MixingPsi<Tag,Time,TimeError>& operator=( MixingPsi<Tag,Time,TimeError> const& other)
+	{
+		if(this==&other) return *this;
+		super_type::operator=(other);
+		return *this;
+	}
+
+	__hydra_dual__ inline
+	std::complex<double> Evaluate(Time t, TimeError sigma_t) const
+	{
+		double chi = (t-b()) / (s()*sigma_t);
+		if (Tag == PSI_P) {
+			double y = suffix();
+			double kappa_p = (1+y)*Gamma()*s()*sigma_t;
+			double result = _psi(chi, kappa_p); // calling _psi<double>()
+			return result; // convert double to std::complex<double> automatically, when returning.
+
+		} else if (Tag == PSI_M) {
+			double y = suffix();
+			double kappa_m = (1-y)*Gamma()*s()*sigma_t;
+			double result = _psi(chi, kappa_m);
+			return result;
+
+		} else if (Tag == PSI_I) {
+			double x = suffix();
+			std::complex<double> kappa_i(Gamma()*s()*sigma_t, x*Gamma()*s()*sigma_t); 
+			std::complex<double> result = _psi(chi, kappa_i);
+			return result;
+		}
+
+		std::cout << "MixingPsi: " << "The Tag(" << Tag << ") is not PSI_P(" << PSI_P << "), PSI_M(" << PSI_M << ") or PSI_I(" << PSI_I << ")" << std::endl;  
+		std::cout << "Something must be wrong! Exit!" << std::endl;
+		exit(-1);
+		return -9999;
+	}
+};
+
+template<typename Time, typename TimeError>
+auto MixingPsip(hydra::Parameter const& suffix, hydra::Parameter const& tau, hydra::Parameter const& s, hydra::Parameter const& b)
+{
+	return MixingPsi<PSI_P,Time,TimeError>(suffix,tau,s,b);
+}
+
+template<typename Time, typename TimeError>
+auto MixingPsim(hydra::Parameter const& suffix, hydra::Parameter const& tau, hydra::Parameter const& s, hydra::Parameter const& b)
+{
+	return MixingPsi<PSI_M,Time,TimeError>(suffix,tau,s,b);
+}
+
+template<typename Time, typename TimeError>
+auto MixingPsii(hydra::Parameter const& suffix, hydra::Parameter const& tau, hydra::Parameter const& s, hydra::Parameter const& b)
+{
+	return MixingPsi<PSI_I,Time,TimeError>(suffix,tau,s,b);
+}
+
+
+// the analytical normalization functor for time_dependent_rate_with_time_resolution_pdf_type1
+template<typename TimeError, typename Signature=double(TimeError)>
+class MixingNormType1: public hydra::BaseFunctor<MixingNormType1<TimeError>, Signature, 5>
+{
+	typedef hydra::BaseFunctor<MixingNormType1<TimeError>, Signature, 5> super_type;
+	using super_type::_par;
+
+private:
+	__hydra_dual__ inline
+	double x() const { return _par[0]; }
+
+	__hydra_dual__ inline
+	double y() const { return _par[1]; }
+
+	__hydra_dual__ inline
+	double tau() const { return _par[2]; }
+
+	__hydra_dual__ inline
+	double Gamma() const { return 1./_par[2]; }
+
+	__hydra_dual__ inline
+	double s() const { return _par[3]; }
+
+	__hydra_dual__ inline
+	double b() const { return _par[4]; }
+
+public:
+	MixingNormType1() = delete;
+     
+	__hydra_dual__
+	MixingNormType1(const double AsumD2Sq_int, const double AdiffD2Sq_int, const std::complex<double> AsumD2AdiffD2Star_int, hydra::Parameter const& x, hydra::Parameter const& y, hydra::Parameter const& tau, hydra::Parameter const& s, hydra::Parameter const& b, std::array<double,2> const& timeRange) : super_type({x,y,tau,s,b}), fAsumD2Sq_int(AsumD2Sq_int), fAdiffD2Sq_int(AdiffD2Sq_int), fAsumD2AdiffD2Star_int(AsumD2AdiffD2Star_int), _timeRange(timeRange)
+	{}
+	
+	__hydra_dual__
+	MixingNormType1(MixingNormType1<TimeError> const& other) : super_type(other), fAsumD2Sq_int(other.fAsumD2Sq_int), fAdiffD2Sq_int(other.fAdiffD2Sq_int), fAsumD2AdiffD2Star_int(other.fAsumD2AdiffD2Star_int), _timeRange(other._timeRange)
+	{}
+	
+	__hydra_dual__ inline
+	MixingNormType1<TimeError>& operator=( MixingNormType1<TimeError> const& other)
+	{
+		if(this==&other) return *this;
+		super_type::operator = (other);
+		fAsumD2Sq_int = other.fAsumD2Sq_int; 
+		fAdiffD2Sq_int = other.fAdiffD2Sq_int; 
+		fAsumD2AdiffD2Star_int = other.fAsumD2AdiffD2Star_int;
+		_timeRange = other._timeRange;
+		return *this;
+	}
+
+	__hydra_dual__ inline
+	double Evaluate(TimeError sigma_t) const 
+	// The dalitz variables and decay-time are integrated, leaving only the sigma_t
+	{
+
+		double sigma = s()*sigma_t;
+		double chi0 = (_timeRange[0]-b()) / (sigma);
+		double chi1 = (_timeRange[1]-b()) / (sigma);
+
+		double kappa_p = (1+y())*Gamma()*sigma;
+		double kappa_m = (1-y())*Gamma()*sigma;
+		std::complex<double> kappa_i(Gamma()*sigma, x()*Gamma()*sigma); 
+		
+		double first_term = fAsumD2Sq_int * (_int_psi_dt(sigma, chi1, kappa_p) - _int_psi_dt(sigma, chi0, kappa_p));
+		double second_term = fAdiffD2Sq_int * (_int_psi_dt(sigma, chi1, kappa_m) - _int_psi_dt(sigma, chi0, kappa_m));
+		double third_term = 2 * (fAsumD2AdiffD2Star_int * (_int_psi_dt(sigma, chi1, kappa_i) - _int_psi_dt(sigma, chi0, kappa_i))).real();
+
+		return first_term + second_term + third_term;
+	}
+
+private:
+	// member variables to store the integration for time dependent parts for the first, second and third term
+	double fAsumD2Sq_int;
+	double fAdiffD2Sq_int;
+	std::complex<double> fAsumD2AdiffD2Star_int;
+
+	std::array<double,2> _timeRange;
+};
+
+
+// We can not use:
+// auto johnson_su = hydra::make_pdf(
+// 					hydra::JohnsonSU<DecayTimeError>(johnson_gamma, johnson_delta, johnson_xi, johnson_lambda), 
+// 					hydra::AnalyticalIntegral<hydra::JohnsonSU<DecayTimeError>>(phsp.TimeErrorMin(), phsp.TimeErrorMax()) 
+// 					);
+// when building the T(mSqP, mSqM, Time, TimeError), because the compose() could not take Pdf class as input
+template<typename ArgType, typename Signature=double(ArgType)>
+class NormalizedJohnsonSU: public hydra::BaseFunctor<NormalizedJohnsonSU<ArgType>, Signature, 4>
+{
+	using hydra::BaseFunctor<NormalizedJohnsonSU<ArgType>, Signature, 4>::_par;
+
+public:
+
+	NormalizedJohnsonSU()=delete;
+
+	NormalizedJohnsonSU(hydra::Parameter const& gamma, hydra::Parameter const& delta, 
+			hydra::Parameter const& xi, hydra::Parameter const& lambda, double const LowerLimit, double UpperLimit):
+			hydra::BaseFunctor<NormalizedJohnsonSU<ArgType>, Signature, 4>({gamma, delta, xi, lambda}),
+			fLowerLimit(LowerLimit), fUpperLimit(UpperLimit)
+			{}
+
+	__hydra_host__ __hydra_device__
+	NormalizedJohnsonSU(NormalizedJohnsonSU<ArgType> const& other ):
+			hydra::BaseFunctor<NormalizedJohnsonSU<ArgType>, Signature, 4>(other), 
+			fLowerLimit(other.fLowerLimit), fUpperLimit(other.fUpperLimit)
+			{}
+
+	__hydra_host__ __hydra_device__
+	NormalizedJohnsonSU<ArgType>&
+	operator=(NormalizedJohnsonSU<ArgType> const& other ){
+		if(this==&other) return  *this;
+		hydra::BaseFunctor<NormalizedJohnsonSU<ArgType>, Signature, 4>::operator=(other);
+		fLowerLimit = other.fLowerLimit;
+		fUpperLimit = other.fUpperLimit;
+		return  *this;
+	}
+
+
+	inline double cumulative( const double gamma,  const double delta,  const double xi,  const double lambda, const double x) const
+	{
+		//actually only 1/lambda is used
+		double inverse_lambda = 1.0/lambda;
+
+		// z =  (x-xi)/lambda
+		double z    = (x-xi)*inverse_lambda;
+
+		// C = {(\gamma + \delta * \asinh(z) )}
+		double C = gamma + delta*::asinh(z);
+
+		return 0.5*(1.0 + ::erf(C*hydra::math_constants::inverse_sqrt2));
+	}
+
+
+	__hydra_host__ __hydra_device__
+	inline double Evaluate(ArgType x)  const
+	{
+		//gathering parameters
+		double gamma  = _par[0];
+		double delta  = _par[1];
+		double xi     = _par[2];
+		//actually only 1/lambda is used
+		double inverse_lambda = 1.0/_par[3];
+
+		// z =  (x-xi)/lambda
+		double z    = (x-xi)*inverse_lambda;
+
+		// A = \frac{\delta}{ \lambda * \sqrt{2\pi} }
+		double A = inverse_lambda*delta*hydra::math_constants::inverse_sqrt2Pi;
+
+		//B = \frac{1}{\sqrt{1 + z^2}}
+		double B = 1.0/::sqrt( 1 + z*z);
+
+		// C = {(\gamma + \delta * \asinh(z) )}^{2}
+		double C = gamma + delta*::asinh(z); C *=C;
+
+		double r = cumulative(_par[0], _par[1], _par[2], _par[3], fUpperLimit)
+ 				 - cumulative(_par[0], _par[1], _par[2], _par[3], fLowerLimit);
+
+		double result = A*B*::exp(-0.5*C) / r;
+
+		return result;
+
+		// return CHECK_VALUE(result, "par[0]=%f, par[1]=%f, par[2]=%f, par[3]=%f", _par[0], _par[1], _par[2], _par[3]  );
+
+
+	}
+
+private:
+	double fLowerLimit;
+	double fUpperLimit;
+
+
+
+};
+
+
 } // namespace dafne
+
+
+
+// The RngFormula of NormalizedJohnsonSU and JohnsonSU should be the same
+template<typename ArgType>
+struct hydra::RngFormula< dafne::NormalizedJohnsonSU<ArgType> >
+{
+
+	typedef ArgType value_type;
+	__hydra_host__ __hydra_device__
+	inline unsigned NCalls( dafne::NormalizedJohnsonSU<ArgType>const&) const
+	{
+		return 1;
+	}
+
+	template< typename T>
+	__hydra_host__ __hydra_device__
+	inline unsigned NCalls( std::initializer_list<T>) const
+	{
+		return 1;
+	}
+
+
+	template<typename Engine>
+	__hydra_host__ __hydra_device__
+	inline value_type Generate(Engine& rng, dafne::NormalizedJohnsonSU<ArgType>const& functor) const
+	{
+		double gamma  = functor[0];
+		double delta  = functor[1];
+		double xi     = functor[2];
+		double lambda = functor[3];
+
+		return static_cast<value_type>(::sinh((nci(RngBase::uniform(rng)) -gamma)/delta )*lambda + xi);
+	}
+
+	template<typename Engine, typename T>
+	__hydra_host__ __hydra_device__
+	inline 	value_type Generate(Engine& rng, std::initializer_list<T> pars) const
+	{
+		double gamma  = pars[0];
+		double delta  = pars[1];
+		double xi     = pars[2];
+		double lambda = pars[3];
+
+		return static_cast<value_type>(::sinh((nci(RngBase::uniform(rng)) -gamma)/delta )*lambda + xi);
+
+	}
+private:
+	__hydra_host__ __hydra_device__
+	inline double nci(double x) const
+		{
+			static const double sqrt_two         = 1.4142135623730950488017;
+
+			return sqrt_two *(hydra::erfinv(2*x-1));
+		}
+
+};
+
+
+
+
--- include/physics/Rate.h
+++ include/physics/Rate.h
@@ -3,8 +3,18 @@
 #include <hydra/Function.h>
 #include <hydra/FunctorArithmetic.h>
 #include <hydra/Lambda.h>
+#include <hydra/Pdf.h>
+#include <hydra/Plain.h>
 
 #include <physics/Mixing.h>
+
+#include <hydra/LogLikelihoodFCN.h>
+#include <hydra/Vector4R.h>
+#include <hydra/multivector.h>
+#include <hydra/Integrator.h>
+
+#include "RooMathM.h"
+
 
 namespace dafne {
 
@@ -48,5 +58,254 @@ auto time_dependent_rate(hydra::Parameter const& tau, hydra::Parameter const& x,
 	auto amp = gp * Adir + rcp * gm * Abar;
 	return rate(amp);
 }
+
+
+template<typename Amplitude>
+auto real_part(Amplitude const& amplitude)
+{
+	auto _real = hydra::wrap_lambda(
+			  [=] __hydra_dual__ (hydra::complex<double> amp){
+						 return amp.real();
+			  }
+	);
+	
+	return hydra::compose(_real, amplitude);
+}
+
+template<typename Amplitude>
+auto imag_part(Amplitude const& amplitude)
+{
+	auto _imag = hydra::wrap_lambda(
+			  [=] __hydra_dual__ (hydra::complex<double> amp){
+						 return amp.imag();
+			  }
+	);
+	
+	return hydra::compose(_imag, amplitude);
+}
+
+
+template<typename Amplitude>
+auto conjugate(Amplitude const& amplitude)
+{
+	auto _conj = hydra::wrap_lambda(
+			  [=] __hydra_dual__ (hydra::complex<double> amp){
+						 return hydra::conj(amp);
+			  }
+	);
+	
+	return hydra::compose(_conj, amplitude);
+}
+
+template<typename Amplitude>
+auto divideBy(Amplitude const& amplitude, const double denominator)
+{
+	auto _divide = hydra::wrap_lambda(
+			  [=] __hydra_dual__ (hydra::complex<double> amp){
+						 return amp / denominator;
+			  }
+	);
+	
+	return hydra::compose(_divide, amplitude);
+}
+
+template<typename Amplitude>
+auto multiplyBy(Amplitude const& amplitude, const double factor)
+{
+	auto _multiply = hydra::wrap_lambda(
+			  [=] __hydra_dual__ (hydra::complex<double> amp){
+						 return amp * factor;
+			  }
+	);
+	
+	return hydra::compose(_multiply, amplitude);
+}
+
+
+template<typename BACKEND>
+struct ConstantIntegrator;
+
+template<hydra::detail::Backend BACKEND>
+class ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>>: 
+public hydra::Integral<ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>>>
+{
+	typedef hydra::detail::BackendPolicy<BACKEND> system_t;
+public:
+	ConstantIntegrator()=delete;
+
+	ConstantIntegrator(double norm):fNorm(norm){};
+
+	// double is not allow to be template parameter, therefore we need to treat the norm as member variables
+	ConstantIntegrator( ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>> const& other):fNorm(other.fNorm){}
+
+	ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>>&
+	operator=( ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>> const& other){fNorm = other.fNorm;}
+
+	template<hydra::detail::Backend BACKEND2>
+	ConstantIntegrator( ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND2>> const& other):fNorm(other.fNorm){}
+
+	template<hydra::detail::Backend BACKEND2>
+	ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND>>&
+    operator=( ConstantIntegrator<hydra::detail::BackendPolicy<BACKEND2>> const& other){fNorm = other.fNorm;}
+
+    template<typename FUNCTOR> 
+    inline std::pair<double, double> Integrate(FUNCTOR const& fFunctor) 
+    {
+    	return std::make_pair(fNorm, 0.0); // constant has no error
+    }
+private:
+	const double fNorm;
+};
+
+
+// This function would build a pdf functor (the funtor is a pdf iself!) for time dependent amplitude with fixed Adir and Abar, and normalized pdf(sigma_t).
+// Currently, q/p is not considered.
+template<Flavor Tag, typename MSqPlus, typename MSqMinus, typename Time, typename TimeError, typename EFFICIENCY, typename ADIR, typename ABAR, typename PDFSIGMAT>
+auto time_dependent_rate_with_time_resolution_pdf(hydra::Parameter const& tau, hydra::Parameter const& x, hydra::Parameter const& y, hydra::Parameter const& qop, hydra::Parameter const& phi, hydra::Parameter const& b, hydra::Parameter const& s, EFFICIENCY const& efficiency, ADIR const& Adir, ABAR const& Abar, PDFSIGMAT const& pdf_sigma_t, std::array<double,2> dalitzRangePlus, std::array<double,2> dalitzRangeMinus, std::array<double,2> timeRange, std::array<double,2> timeErrorRange, const long n_calls=50000000)
+{
+
+	// check if Adir and Abar are fixed
+	std::cout << "Checking if Adir and Abar are fixed ... ..." << std::endl;
+
+	// make a dummy fcn to easily get the parameter list
+	hydra::multivector<hydra::tuple<MSqPlus,MSqMinus>, hydra::device::sys_t> data_dummy;
+	data_dummy.push_back(hydra::make_tuple(MSqPlus(1.0),MSqMinus(1.0)));
+
+	auto AdirDummyFCN = hydra::make_loglikehood_fcn( hydra::make_pdf( rate(Adir), ConstantIntegrator<hydra::device::sys_t>(1.0)) ,
+													 data_dummy.begin(),
+													 data_dummy.end());
+	auto AbarDummyFCN = hydra::make_loglikehood_fcn( hydra::make_pdf( rate(Abar), ConstantIntegrator<hydra::device::sys_t>(1.0)) ,
+													 data_dummy.begin(),
+													 data_dummy.end());
+
+    auto AdirParameters = AdirDummyFCN.GetParameters().GetVariables();
+    auto AbarParameters = AbarDummyFCN.GetParameters().GetVariables();
+
+    auto check_free_parameters = [](std::vector<hydra::Parameter* >& parameters, const char* amplitudeName) {
+    	int n_free = 0;
+	    std::string free_parameter_str("");
+	    for (size_t i = 0; i < parameters.size(); ++i) {
+	        if (!(parameters[i]->IsFixed())) {
+	        	n_free++;
+	        	free_parameter_str += " " + std::string(parameters[i]->GetName());
+	        }
+	    }
+
+	    if (n_free != 0) {
+	    	std::cout << "time_dependent_rate_with_time_resolution_pdf_type1: ";
+	    	std::cout << amplitudeName << " should be fixed! But now it has " << n_free << " floating parameters: " << std::endl;
+	    	std::cout << free_parameter_str << std::endl;
+	        exit(-1);
+    	}
+    };
+
+    check_free_parameters(AdirParameters, "Adir");
+    check_free_parameters(AbarParameters, "Abar");
+
+	std::cout << "Pass." << std::endl;
+
+	// check if pdf(sigma_t) is normalized
+	std::cout << "Checking if pdf(sigma_t) is normalized ... ..." << std::endl;
+
+	hydra::Plain<1,  hydra::device::sys_t > PlainSigmatMC({timeErrorRange[0]}, {timeErrorRange[1]}, 5000000);
+	double int_pdf_sigma_t = PlainSigmatMC.Integrate(pdf_sigma_t).first;
+	std::cout << "Integrate(pdf(sigma_t)) on (timeErrorRange[0], timeErrorRange[1]) = " << int_pdf_sigma_t << std::endl;
+	// double int_pdf_sigma_t = pdf_sigma_t.GetNorm();
+	// std::cout << "pdf_sigma_t.GetNorm() = " << int_pdf_sigma_t << std::endl;
+	if (abs(int_pdf_sigma_t - 1) > 0.01) {
+		std::cout << "time_dependent_rate_with_time_resolution_pdf: ";
+		std::cout << "The input pdf(sigma_t) should be normalized, but its integration seems not close enough to 1. ";
+		std::cout << "Exit!" << std::endl; 
+	}
+
+	std::cout << "Pass." << std::endl;
+
+	// integrate on dalitz plane and decay-time axis
+	hydra::Plain<2,  hydra::device::sys_t > PlainDalitzMC({dalitzRangePlus[0], dalitzRangeMinus[0]}, 
+														  {dalitzRangePlus[1], dalitzRangeMinus[1]}, 
+														  n_calls);
+
+	// numerically integrate for time-independent part
+	// suppose the efficiency plane is time-independent and has not correlation with decay-time or sigmat.
+
+	auto rcp = QoverP<Tag>(qop,phi);
+
+	auto AlphaSq = efficiency * rate(Adir);
+	auto BetaSq = efficiency * rate(rcp * Abar);
+	auto AlphaBetaStar_real = efficiency * real_part(Adir * conjugate(rcp * Abar)); 
+	auto AlphaBetaStar_imag = efficiency * imag_part(Adir * conjugate(rcp * Abar)); 
+	
+	double AlphaSq_int = PlainDalitzMC.Integrate(AlphaSq).first;
+	double BetaSq_int = PlainDalitzMC.Integrate(BetaSq).first;
+	double AlphaBetaStar_real_int = PlainDalitzMC.Integrate(AlphaBetaStar_real).first;
+	double AlphaBetaStar_imag_int = PlainDalitzMC.Integrate(AlphaBetaStar_imag).first;
+
+	double AsumD2Sq_int = 1. / 4. * ( AlphaSq_int + BetaSq_int + 2*AlphaBetaStar_real_int );
+	double AdiffD2Sq_int = 1. / 4. * ( AlphaSq_int + BetaSq_int - 2*AlphaBetaStar_real_int );
+	hydra::complex<double> AsumD2AdiffD2Star_int = 1. / 4. * hydra::complex<double>( AlphaSq_int - BetaSq_int , -2*AlphaBetaStar_imag_int );
+
+
+	// build the final functor
+
+	auto psi_p = MixingPsip<Time, TimeError>(y,tau,s,b);
+	auto psi_m = MixingPsim<Time, TimeError>(y,tau,s,b);
+	auto psi_i = MixingPsii<Time, TimeError>(x,tau,s,b);
+
+	auto As = Adir + rcp * Abar; 
+	auto Ad = conjugate( Adir - rcp * Abar ) ; 
+
+	auto int_on_dalitzplane_decaytime = MixingNormType1<TimeError>(AlphaSq_int, BetaSq_int, AsumD2AdiffD2Star_int, 
+																	  	 x, y, tau, s, b, timeRange);
+
+	// the hydra::wrap_lambda and hydra::compose() below would handle the parameter list of Adir and Abar automatically
+	auto _arrange = hydra::wrap_lambda(
+			  [] __hydra_dual__ (hydra::tuple< double, hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, double, double > input_amplitudes){
+			  		auto _efficiency = hydra::get<0>(input_amplitudes);
+			  		auto _As = hydra::get<1>(input_amplitudes);
+			  		auto _Ad = hydra::get<2>(input_amplitudes);
+			  		auto _psi_p = hydra::get<3>(input_amplitudes);
+			  		auto _psi_m = hydra::get<4>(input_amplitudes);
+			  		auto _psi_i = hydra::get<5>(input_amplitudes);
+			  		auto _pdf_sigma_t = hydra::get<6>(input_amplitudes);
+			  		auto _int_on_dalitzplane_decaytime = hydra::get<7>(input_amplitudes);
+
+			  		return  _efficiency * ( norm(_As/2.)*_psi_p.real() + norm(_Ad/2.)*_psi_m.real() + (_As*_Ad/2.*_psi_i).real() ) * _pdf_sigma_t / _int_on_dalitzplane_decaytime;
+
+			  }
+	);
+	
+	return hydra::compose(_arrange, efficiency, As, Ad, psi_p, psi_m, psi_i, pdf_sigma_t, int_on_dalitzplane_decaytime);
+
+
+	// auto _arrange = hydra::wrap_lambda(
+	// 	  [=] __hydra_dual__ (hydra::tuple< hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, hydra::complex<double>, double > input_amplitudes){
+	// 	  		auto _As = hydra::get<0>(input_amplitudes);
+	// 	  		auto _Ad = hydra::get<1>(input_amplitudes);
+	// 	  		auto _psi_p = hydra::get<2>(input_amplitudes);
+	// 	  		auto _psi_m = hydra::get<3>(input_amplitudes);
+	// 	  		auto _psi_i = hydra::get<4>(input_amplitudes);
+	// 	  		auto _pdf_sigma_t = hydra::get<5>(input_amplitudes);
+
+	// 	  		return  ( norm(_As/2.)*_psi_p.real() + norm(_Ad/2.)*_psi_m.real() + (_As*_Ad/2.*_psi_i).real() ) * _pdf_sigma_t ;
+
+	// 	  }
+	// );
+	
+	// return hydra::compose(_arrange, As, Ad, psi_p, psi_m, psi_i, pdf_sigma_t);
+
+}
+
+
+// This function would build a pdf functor for time dependent amplitude with unfixed Adir and Abar, and normalized pdf(sigma_t).
+// template<Flavor Tag, typename MSqPlus, typename MSqMinus, typename Time, typename TimeError, typename ADIR, typename ABAR, typename PDFSIGMAT>
+// dummy time_dependent_rate_with_time_resolution_pdf_type2; // to be developed. Probably we need a new functor with _integrated_Adir 
+// and _integrated_Abar as member variables, and update this member variables when it finds the fAdir/fAbar parameter list is changed, 
+// while using this _integrated_Adir and _integrated_Abar in the evaluate() when calculating normalization factor. This class/function 
+// would be developed in the future if needed
+
+
+
+
+
 
 } // namespace dafne
